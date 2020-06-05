c2681b13d1f9d90c3bc8812d88a8936c556e7b76
Hashirama <davidiwobi@icloud.com>
2020-06-05 14:59:46 +0100

    Add files via upload

--- OFDL/module/OF.py
+++ OFDL/module/OF.py
@@ -5,11 +5,13 @@ import os
 import sqlite3
 from sqlite3 import Error
 
-MESSAGES = 0b10000
-PICTURES = 0b01000
-VIDEOS = 0b00100
-HIGHLIGHTS = 0b00010
-STORIES = 0b00001
+MESSAGES = 0b1000000
+PICTURES = 0b0100000
+VIDEOS = 0b0010000
+HIGHLIGHTS = 0b0001000
+STORIES = 0b0000100
+ARCHIVED = 0b0000010
+AUDIO = 0b0000001
 
 class Onlyfans:
     def __init__(self):
@@ -63,19 +65,32 @@ class Onlyfans:
                 "https://onlyfans.com/api2/v2/users/me?app-token=" + self.app_token)
         if r.status_code != 200:
             print ("Login failed")
+            print (r.content)
             return
+        
         r = self.session.get(
                 "https://onlyfans.com/api2/v2/subscriptions/count/all?app-token="+self.app_token)
-        subscription_link = "https://onlyfans.com/api2/v2/subscriptions/subscribes?limit=99&offset=0&app-token="+self.app_token
-        json_result = self.session.get(subscription_link)
-        json_result = json.loads(json_result.content)
+        r = json.loads(r.text)
+        count = r["subscriptions"]["all"]
 
-        for sub in json_result:
-            if sub["subscribedBy"] == True:
-                self.active_subs.append(sub["username"])
-            else:
-                self.expired_subs.append(sub["username"])
-            self.all_subs.append(sub["username"])
+        offset_range = math.ceil(count / 99)
+        offsets = list(range(offset_range))
+
+        subscription_link = "https://onlyfans.com/api2/v2/subscriptions/subscribes?limit=99&offset=&app-token=" + self.app_token
+
+        for off in offsets:
+            offset_str = off * 99
+            sub_temp = subscription_link.replace("offset=", "offset=" + str(offset_str))
+            json_result = self.session.get(sub_temp)
+            json_result = json.loads(json_result.content)
+            for sub in json_result:
+                if sub["subscribedBy"] == True and sub["username"] not in self.active_subs:
+                    self.active_subs.append(sub["username"])
+                else:
+                    if sub["username"] not in self.expired_subs:
+                        self.expired_subs.append(sub["username"])
+                if sub["username"] not in self.all_subs:
+                    self.all_subs.append(sub["username"])
 
 
     def return_active_subs(self):
@@ -100,6 +115,7 @@ class Onlyfans:
         return_dict["photosCount"] = json_data["photosCount"]
         return_dict["videosCount"] = json_data["videosCount"]
         return_dict["audiosCount"] = json_data["audiosCount"]
+        return_dict["archivedPostsCount"] = json_data["archivedPostsCount"]
         return_dict["id"] = json_data["id"]
         return_dict["username"] = username
 
@@ -122,6 +138,7 @@ class Onlyfans:
         audio = []
         stories = []
         highlights = []
+        archived = []
         total_count = info["photosCount"] + info["videosCount"] + info["audiosCount"]
 
         offset_range = math.ceil(total_count / 100)
@@ -137,8 +154,15 @@ class Onlyfans:
         stories_api = "https://onlyfans.com/api2/v2/users/"+ str(user_id) + \
         "/stories?limit=100&offset=0&order=desc&app-token="+ self.app_token
 
+        archive_api = "https://onlyfans.com/api2/v2/users/"+ str(user_id) + \
+        "/posts/archived?limit=100&offset=0&order=publish_date_desc&app-token=" + self.app_token
+
         highlight_api = "https://onlyfans.com/api2/v2/users/" + str(user_id) + \
                      "/stories/highlights?app-token=" + self.app_token
+
+
+        audio_api = "https://onlyfans.com/api2/v2/users/" + str(user_id) + \
+                    "/posts/audios?limit=10&offset=&order=publish_date_desc&app-token=" + self.app_token
 
 
         if flag & HIGHLIGHTS:
@@ -164,6 +188,10 @@ class Onlyfans:
                                         if "source" in src_json:
                                             src = src_json["source"]
                                             file_size = src_json["size"]
+                                            if file_size == 0:
+                                                r = self.session.head(src)
+                                                if "Content-Length" in r.headers:
+                                                    file_size = int(r.headers["Content-Length"])
                                             self.all_files_size += file_size
                                             file_dict = {"source" : src, "size": file_size, "index" : index, "id": story_id,
                                                              "date" : date, "flag" : HIGHLIGHTS}
@@ -199,6 +227,11 @@ class Onlyfans:
                                     m_info = m["info"]
                                     m_info = m_info["source"]
                                     file_size = m_info["size"]
+                                    
+                                    if file_size == 0:
+                                        r = self.session.head(src)
+                                        if "Content-Length" in r.headers:
+                                            file_size = int(r.headers["Content-Length"])
                                     if type_src == "photo":
                                         self.all_files_size += file_size
                                         file_dict = {"source" : src, "size": file_size, "index" : index, "id": id_post,
@@ -236,6 +269,11 @@ class Onlyfans:
                             if "source" in file_details and "size" in file_details:
                                 src = file_details["source"]
                                 file_size = file_details["size"]
+
+                                if file_size == 0:
+                                    r = self.session.head(src)
+                                    if "Content-Length" in r.headers:
+                                        file_size = int(r.headers["Content-Length"])
                                 file_dict = {"source" : src, "size" : file_size, "index" : index, "id" : post_id,
                                              "date" : date, "flag" : STORIES}
                                 stories.append(file_dict)
@@ -258,6 +296,10 @@ class Onlyfans:
                                     type_src = file_details["source"]
                                     if type_src is None or not type_src.startswith('https://cdn'):
                                         continue
+                                    if file_size == 0:
+                                        r = self.session.head(type_src)
+                                        if "Content-Length" in r.headers:
+                                            file_size = int(r.headers["Content-Length"])
                                     if ".jpg" in type_src or ".jpeg" in type_src or ".png" in type_src:
                                         self.all_files_size += file_size
                                         file_dict = {"source" : type_src, "size": file_size, "index" : index, "id": id_post,
@@ -280,18 +322,99 @@ class Onlyfans:
                                     type_src = file_details["source"]
                                     if type_src is None or not type_src.startswith('https://cdn'):
                                         continue
+
+                                    if file_size == 0:
+                                        r = self.session.head(type_src)
+                                        if "Content-Length" in r.headers:
+                                            file_size = int(r.headers["Content-Length"])      
+                                    
                                     if ".mp4" in type_src:
                                         self.all_files_size += file_size
                                         file_dict = {"source" : type_src, "size": file_size, "index" : index, "id": id_post,
                                              "date" : date, "flag" : VIDEOS}
                                         videos.append(file_dict)
+
+        if flag & ARCHIVED:
+             cnt = info["archivedPostsCount"]
+             off_range = math.ceil(cnt / 100)
+             offsets_arch = list(range(off_range))
+
+             for off in offsets_arch:
+                 arch_temp = archive_api.replace("offset=", "offset=" + str(off))
+                 r = self.session.get(arch_temp)
+                 js = json.loads(r.text)
+
+                 for j in js:
+                     if "postedAt" in j and "media" in j and "id" in j:
+                         date = j["postedAt"]
+                         media = j["media"]
+                         id_post = j["id"]
+                         for m in media:
+                             if "source" in m:
+                                 file_details = m["source"]
+                                 if "size" in file_details and "source" in file_details:
+                                     file_size = file_details["size"]
+                                     type_src = file_details["source"]
+                                 if type_src is None or not type_src.startswith('https://cdn'):
+                                        continue
+
+                                 if file_size == 0:
+                                     r = self.session.head(type_src)
+                                     if "Content-Length" in r.headers:
+                                         file_size = int(r.headers["Content-Length"])
+                                 if ".mp4" in type_src:
+                                        self.all_files_size += file_size
+                                        file_dict = {"source" : type_src, "size": file_size, "index" : index, "id": id_post,
+                                             "date" : date, "flag" : ARCHIVED}
+                                        archived.append(file_dict)
+                                 if ".jpg" in type_src or ".jpeg" in type_src or ".png" in type_src:
+                                        self.all_files_size += file_size
+                                        file_dict = {"source" : type_src, "size": file_size, "index" : index, "id": id_post,
+                                             "date" : date, "flag" : ARCHIVED}
+                                        archived.append(file_dict)
+
+
+        if flag & AUDIO:
+            cnt = info["audiosCount"]
+            off_range = math.ceil(cnt / 10)
+            offsets_arch = list(range(off_range))
+
+            for off in offsets_arch:
+                 audio_api = audio_api.replace("offset=", "offset=" + str(off))
+                 r = self.session.get(audio_api)
+                 js = json.loads(r.text)
+
+                 for j in js:
+                     if "postedAt" in j and "media" in j and "id" in j:
+                         date = j["postedAt"]
+                         media = j["media"]
+                         id_post = j["id"]
+                         for m in media:
+                             if "source" in m:
+                                 file_details = m["source"]
+                                 if "size" in file_details and "source" in file_details:
+                                     file_size = file_details["size"]
+                                     type_src = file_details["source"]
+                                 if type_src is None or not type_src.startswith('https://cdn'):
+                                        continue
+                                 if ".mp3" in type_src:
+                                    self.all_files_size += file_size
+                                    file_dict = {"source" : type_src, "size": file_size, "index" : index, "id": id_post,
+                                                 "date" : date, "flag" : AUDIO}
+                                    audio.append(file_dict)
+                                 
+                         
+                 
+                                 
+                                     
+                
+            
                             
-        self.links += stories + highlights + images + videos + audio
+        self.links += stories + highlights + images + videos + audio + archived
 
         list_copy = self.links.copy()
 
-
-        for link in list_copy:
+        for link in list_copy:    
             filename = link["source"].split('/')[-1]
             filename = filename.split('?')[0]
             if self.select_database(user_id, filename) == True:
@@ -366,6 +489,8 @@ class Onlyfans:
             directory = "Highlights"
         elif flag & STORIES:
             directory = "Stories"
+        elif flag & ARCHIVED:
+            directory = "Archived"
 
         self.create_dir(folder + "/" + directory)
 
@@ -373,43 +498,63 @@ class Onlyfans:
 
         if File_Extension == "jpg" or File_Extension == "jpeg" or File_Extension == "png":
             self.create_dir(folder + "/" + directory + "/Images")
-            with open("Files/" + folder + "/" + directory + "/Images/" + File_Name, "wb") as file:
-                response = self.session.get(file_name, stream=True)
-                tmp = response.headers.get('content-length')
-                if tmp is None:
-                    file.write(response.content)
-                else:
-                    total_length = int(tmp)
-                    for data in response.iter_content(chunk_size=4096):
-                        self.current_dl += len(data)
-                        obj.ProgressBar['value'] =  (self.current_dl / total_download) * 100
-                        file.write(data)
+            if os.path.isfile("Files/" + folder + "/" + directory + "/Images/" + File_Name) == False:
+                with open("Files/" + folder + "/" + directory + "/Images/" + File_Name, "wb") as file:
+                    response = self.session.get(file_name, stream=True)
+                    tmp = response.headers.get('content-length')
+                    if tmp is None:
+                        file.write(response.content)
+                    else:
+                        total_length = int(tmp)
+                        for data in response.iter_content(chunk_size=4096):
+                            self.current_dl += len(data)
+                            obj.ProgressBar['value'] =  (self.current_dl / total_download) * 100
+                            file.write(data)
+                            
         elif File_Extension == "mp4":
             self.create_dir(folder + "/" + directory + "/Videos")
-            with open("Files/" + folder + "/" + directory + "/Videos/" + File_Name, "wb") as file:
-                response = self.session.get(file_name, stream=True)
-                tmp = response.headers.get('content-length')
-                if tmp is None:
-                    file.write(response.content)
-                else:
-                    total_length = int(tmp)
-                    for data in response.iter_content(chunk_size=4096):
-                        self.current_dl += len(data)
-                        obj.ProgressBar['value'] =  (self.current_dl / total_download) * 100
-                        file.write(data)
+            if os.path.isfile("Files/" + folder + "/" + directory + "/Videos/" + File_Name) == False:
+                with open("Files/" + folder + "/" + directory + "/Videos/" + File_Name, "wb") as file:
+                    response = self.session.get(file_name, stream=True)
+                    tmp = response.headers.get('content-length')
+                    if tmp is None:
+                        file.write(response.content)
+                    else:
+                        total_length = int(tmp)
+                        for data in response.iter_content(chunk_size=4096):
+                            self.current_dl += len(data)
+                            obj.ProgressBar['value'] =  (self.current_dl / total_download) * 100
+                            file.write(data)
+                            
+        elif File_Extension == "mp3":
+            self.create_dir(folder + "/" + directory + "/Audio")
+            if os.path.isfile("Files/" + folder + "/" + directory + "/Audio/" + File_Name) == False:
+                with open("Files/" + folder + "/" + directory + "/Audio/" + File_Name, "wb") as file:
+                    response = self.session.get(file_name, stream=True)
+                    tmp = response.headers.get('content-length')
+                    if tmp is None:
+                        file.write(response.content)
+                    else:
+                        total_length = int(tmp)
+                        for data in response.iter_content(chunk_size=4096):
+                            self.current_dl += len(data)
+                            obj.ProgressBar['value'] =  (self.current_dl / total_download) * 100
+                            file.write(data)
+                            
         else:
             self.create_dir(folder + "/" + directory + "/Misc")
-            with open("Files/" + folder + "/" + directory + "/Misc/" + File_Name, "wb") as file:
-                response = self.session.get(file_name, stream=True)
-                tmp = response.headers.get('content-length')
-                if tmp is None:
-                    file.write(response.content)
-                else:
-                    total_length = int(tmp)
-                    for data in response.iter_content(chunk_size=4096):
-                        self.current_dl += len(data)
-                        obj.ProgressBar['value'] =  (self.current_dl / total_download) * 100
-                        file.write(data)
+            if os.path.isfile("Files/" + folder + "/" + directory + "/Misc/" + File_Name) == False:
+                with open("Files/" + folder + "/" + directory + "/Misc/" + File_Name, "wb") as file:
+                    response = self.session.get(file_name, stream=True)
+                    tmp = response.headers.get('content-length')
+                    if tmp is None:
+                        file.write(response.content)
+                    else:
+                        total_length = int(tmp)
+                        for data in response.iter_content(chunk_size=4096):
+                            self.current_dl += len(data)
+                            obj.ProgressBar['value'] =  (self.current_dl / total_download) * 100
+                            file.write(data)
             
 
     def create_dir(self, dirname):
